e5dd0f666fd4a19690a2ad129ed52c3144dfae1a
mcortes19 <marvin.cortes@gmail.com>
2021-05-24 22:06:32 -0600

    V-5: custom hooks.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import ReactDOM from 'react-dom';
-import Pet from './Pet.jsx';
 import SearchParams from './SearchParams.jsx';
 
 export default function App() {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      {/* <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Paper" animal="Bird" breed="Cockatiel" />
-      <Pet name="Sudo" animal="Dog" breed="Wheaten Terrier" /> */}
       <SearchParams />
     </div>
   );
---
+++ src/Results.jsx
@@ -0,0 +1,23 @@
+import Pet from './Pet.jsx';
+
+export default function Results({ pets }) {
+  return (
+    <div className="search">
+      {!pets.length ? (
+        <h2>No Pets Found</h2>
+      ) : (
+        pets.map((pet) => (
+          <Pet
+            key={pet.id}
+            name={pet.name}
+            animal={pet.animal}
+            breed={pet.breed}
+            images={pet.images}
+            location={`${pet.city}, ${pet.state}`}
+            id={pet.id}
+          />
+        ))
+      )}
+    </div>
+  );
+}
